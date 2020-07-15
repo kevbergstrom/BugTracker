87e9e03e835aca11fb523184b996443d0c22c837
kevin <kevbergstrom@gmail.com>
2020-07-14 20:09:01 -0700

    Setup basic routes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const { userRoute } = require('./routes')
 
 const PORT = process.env.PORT || 5000
+
+// routes
+const apiRouter = express.Router()
+app.use('/api', apiRouter)
+apiRouter.use('/user', userRoute )
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
