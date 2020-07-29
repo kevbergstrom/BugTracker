d0cabfa76685dccfff09b8fd7ce195b040c100d4
kevin <kevbergstrom@gmail.com>
2020-07-29 16:29:14 -0700

    Add auth check for sidebar and navbar

--- client/src/components/navigation/navbar.js
+++ client/src/components/navigation/navbar.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Navbar = props => {
+const Navbar = ({ isAuthenticated }) => {
     return (
         <nav className="navbar navbar-expand-lg navbarColor shadow">
             <Link className="nav-link darkText" to="/"><h6>BugTracker</h6></Link>
             <ul className="navbar-nav mr-auto">
             </ul>
-            {/* not logged in */}
-            <Link className="nav-link darkText" to="/signup"><h6>Sign up</h6></Link>
-            <Link className="nav-link darkText" to="/login"><h6>Login</h6></Link>
-            {/* logged in */}
-            <a className="nav-link darkText"><h6>Dashboard</h6></a>
-
+            {isAuthenticated ? 
+                <>
+                    <Link className="nav-link darkText" to="/dashboard"><h6>Dashboard</h6></Link>
+                    <Link className="nav-link darkText" to="/logout"><h6>Logout</h6></Link>
+                </>
+                : <>
+                    <Link className="nav-link darkText" to="/signup"><h6>Sign up</h6></Link>
+                    <Link className="nav-link darkText" to="/login"><h6>Login</h6></Link>
+                </>
+            }
             <Link className="nav-link darkText" to="/about"><h6>About</h6></Link>
             <a className="nav-link darkText" href="https://github.com"><h6>Github</h6></a>
           </nav>
     )
 }
 
-export default Navbar
+const mapStateToProps = state => ({
+    isAuthenticated: state.authReducer.isAuthenticated
+})
+
+export default connect(mapStateToProps)(Navbar)
--- client/src/components/navigation/sidebar.js
+++ client/src/components/navigation/sidebar.js
@@ -1,84 +1,84 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-const Sidebar = props => {
+const Sidebar = ({ user: { username } }) => {
     return(
         <nav className="col-md-2 d-none d-md-block sidebar fixed-top sidebarColor">
             <div className="sidebar-sticky">
-                <a className="nav-link lightText container-fluid sidebarAlt" href="#">
+                <Link className="nav-link lightText container-fluid sidebarAlt" to="">
                     <h6>
                     <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-file-person" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
                         <path d="M13.784 14c-.497-1.27-1.988-3-5.784-3s-5.287 1.73-5.784 3h11.568z"/>
                         <path fill-rule="evenodd" d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                      </svg> Username</h6>
-                </a>
+                    </svg> {username}</h6>
+                </Link>
 
                 <ul className="nav flex-column">
                     <li className="nav-item">
-                        <a className="nav-link lightText sidebarColor" href="#">
+                        <Link className="nav-link lightText sidebarColor" to="/dashboard">
                             <h6>
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-calendar-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"/>
                                     <path d="M2 1a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2H2zm14 4H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5z"/>
-                                </svg>
-                                Dashboard
+                                </svg>&nbsp;Dashboard
                             </h6>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link lightText sidebarColor" href="#">
+                        <Link className="nav-link lightText sidebarColor" to="">
                             <h6>
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-binoculars-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M4.5 1A1.5 1.5 0 0 0 3 2.5V3h4v-.5A1.5 1.5 0 0 0 5.5 1h-1zM7 4v1h2V4h4v.882a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V13H9v-1.5a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5V13H1V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V4h4zM1 14v.5A1.5 1.5 0 0 0 2.5 16h3A1.5 1.5 0 0 0 7 14.5V14H1zm8 0v.5a1.5 1.5 0 0 0 1.5 1.5h3a1.5 1.5 0 0 0 1.5-1.5V14H9zm4-11H9v-.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5V3z"/>
-                                </svg>
-                                Projects
+                                </svg>&nbsp;Projects
                             </h6>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link lightText sidebarColor" href="#">
+                        <Link className="nav-link lightText sidebarColor" to="">
                             <h6>
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-bug-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M4.978.855a.5.5 0 1 0-.956.29l.41 1.352A4.985 4.985 0 0 0 3 6h10a4.985 4.985 0 0 0-1.432-3.503l.41-1.352a.5.5 0 1 0-.956-.29l-.291.956A4.978 4.978 0 0 0 8 1a4.979 4.979 0 0 0-2.731.811l-.29-.956zM13 6v1H8.5v8.975A5 5 0 0 0 13 11h.5a.5.5 0 0 1 .5.5v.5a.5.5 0 1 0 1 0v-.5a1.5 1.5 0 0 0-1.5-1.5H13V9h1.5a.5.5 0 0 0 0-1H13V7h.5A1.5 1.5 0 0 0 15 5.5V5a.5.5 0 0 0-1 0v.5a.5.5 0 0 1-.5.5H13zm-5.5 9.975V7H3V6h-.5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 0-1 0v.5A1.5 1.5 0 0 0 2.5 7H3v1H1.5a.5.5 0 0 0 0 1H3v1h-.5A1.5 1.5 0 0 0 1 11.5v.5a.5.5 0 1 0 1 0v-.5a.5.5 0 0 1 .5-.5H3a5 5 0 0 0 4.5 4.975z"/>
-                                </svg>
-                                Bugs
+                                </svg>&nbsp;Bugs
                             </h6>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link lightText sidebarColor" href="#">
+                        <Link className="nav-link lightText sidebarColor" to="">
                             <h6>
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-envelope-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
-                                </svg>
-                                Invites
+                                </svg>&nbsp;Invites
                             </h6>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link lightText sidebarColor" href="#">
+                        <Link className="nav-link lightText sidebarColor" to="">
                             <h6>
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-person-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
                                     <path fill-rule="evenodd" d="M13 7.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0v-2z"/>
-                                </svg>
-                                Users
+                                </svg>&nbsp;Users
                             </h6>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
-                    <a className="nav-link lightText container-fluid sidebarAlt bottom" href="#">
+                    <Link className="nav-link lightText container-fluid sidebarAlt bottom" to="/logout">
                         <h6>
                             <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-power" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M5.578 4.437a5 5 0 1 0 4.922.044l.5-.866a6 6 0 1 1-5.908-.053l.486.875z"/>
                                 <path fill-rule="evenodd" d="M7.5 8V1h1v7h-1z"/>
-                            </svg>
-                            Logout
+                            </svg>&nbsp;Logout
                         </h6>
-                    </a>
+                    </Link>
             </div>
         </nav>
     )
 }
 
-export default Sidebar
+const mapStateToProps = state => ({
+    user: state.authReducer.user
+})
+
+export default connect(mapStateToProps)(Sidebar)
