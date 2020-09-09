9ed505cd7e17c955d501e23040a7eb96a7bad94a
kevin <kevbergstrom@gmail.com>
2020-09-09 00:10:25 -0700

    Add deployment code

--- index.js
+++ index.js
@@ -54,6 +54,17 @@ apiRouter.use('/project', projectRoute)
 apiRouter.use('/project/:projectId/bug', bugRoute)
 apiRouter.use('/project/:projectId/bug/:bugId/comment', commentRoute)
 
+//for heroku
+//serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+    //serve index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
