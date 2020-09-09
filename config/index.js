fa07cd9a7f0e8218efe7ca95384e2ce033e483aa
kevin <kevbergstrom@gmail.com>
2020-09-09 00:44:41 -0700

    Fix client npm errors

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    MONGO_URI: process.env.MONGO_URI,
-    SESSION_SECRET: process.env.SESSION_SECRET,
+    MONGO_URI: process.env.MONGO_URI || '',
+    SESSION_SECRET: process.env.SESSION_SECRET || '',
     SESSION_LIFETIME: process.env.SESSION_LIFETIME || 1000 * 60 * 2,
     SESSION_NAME: process.env.SESSION_NAME || 'sid',
     PASSWORD_MIN_LENGTH: 8,
