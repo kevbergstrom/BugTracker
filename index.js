ebebff3b79bfca50b4d5c4fb2a30e1a7c29a938e
kevin <kevbergstrom@gmail.com>
2020-09-09 01:00:02 -0700

    Fix path error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
+const path = require('path')
 
 const { userRoute, projectRoute, bugRoute, commentRoute } = require('./routes')
 
